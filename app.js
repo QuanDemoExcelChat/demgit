5d51bd440f230fe85174af6304992d1990648bc3
QuanDemoExcelChat <54097178+QuanDemoExcelChat@users.noreply.github.com>
2019-11-09 23:55:06 +0700

    Add lan 2

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var a = 1;
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,3 +41,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+//added
